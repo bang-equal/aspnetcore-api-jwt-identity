523dc4f5bb1d38d9844f272f79baae32c15b5b92
Herman A. Toro <herman.toro@outlook.com>
2017-03-28 10:02:48 -0400

    Update Startup.cs

--- src/BareMetalApi/Startup.cs
+++ src/BareMetalApi/Startup.cs
@@ -20,7 +20,7 @@ namespace BareMetalApi
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
-                .SetBasePath(Path.Combine(Directory.GetCurrentDirectory(), @"src\BareMetalApi"))
+                .SetBasePath(env.ContentRootPath)
                 .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
